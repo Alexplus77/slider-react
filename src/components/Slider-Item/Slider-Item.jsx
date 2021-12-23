64822e46294be3c3cca995575f50b04d31422c67
alexs <alexplus77@mail.ru>
2021-12-23 18:19:36 +0200

    commit

--- src/components/Slider-Item/Slider-Item.jsx
+++ src/components/Slider-Item/Slider-Item.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-
+import "App.css";
 const SliderItem = ({ img }) => {
   return (
     <div className="slider__item slider__item_active">
-      <img src={img} alt={"..."} />
+      <img className="slider__image" src={img} alt={"..."} />
     </div>
   );
 };
