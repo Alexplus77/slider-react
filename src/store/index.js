86bbb3a7ee283024ad6e1cdeb799e7ebe79fe901
alexs <alexplus77@mail.ru>
2022-01-21 12:52:48 +0200

    commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,9 @@
 import { createStore, combineReducers } from "redux";
 import { changeCount } from "reducers/reducersSlider";
-
+import { devToolsEnhancer } from "redux-devtools-extension";
 const reducers = combineReducers({
   changeCount: changeCount,
 });
 
-const store = createStore(reducers);
+const store = createStore(reducers, devToolsEnhancer());
 export { store };
